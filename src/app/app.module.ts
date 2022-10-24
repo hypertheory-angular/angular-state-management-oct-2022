24de401a300b42df124945002b2d59b0cbe3148b
Jeff Gonzalez <jeff@hypertheory.com>
2022-10-24 12:30:13 -0400

    before lunch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MastheadComponent } from './components/masthead/masthead.component';
+import { DumbCounterComponent } from './components/dumb-counter/dumb-counter.component';
 
 
 
 
 @NgModule({
   declarations: [ // components 
-    AppComponent, MastheadComponent
+    AppComponent, MastheadComponent, DumbCounterComponent
   ],
   imports: [ // use these other modules
     BrowserModule,
---
+++ src/app/components/dumb-counter/dumb-counter.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dumb-counter',
+  templateUrl: './dumb-counter.component.html',
+  styleUrls: ['./dumb-counter.component.css']
+})
+export class DumbCounterComponent  {
+
+  // state - a variable of type number;
+ current = 0;
+
+ // affordances through which the user can change that state
+ increment() {
+  this.current += 1;
+ }
+
+ // affordances through which the user can change that state
+
+ decrement() {
+  this.current -= 1;
+ }
+
+}
