9c2e9efa52623db99541222f58f3924199020187
Jeff Gonzalez <jeff@hypertheory.com>
2022-10-26 14:23:17 -0400

    mid song

--- src/app/features/songs/state/index.ts
+++ src/app/features/songs/state/index.ts
@@ -1,4 +1,9 @@
-import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
+import { SongListModel } from '../models';
 
 import * as fromSongList from './reducers/song-list.reducer';
 export const FEATURE_NAME = 'songsFeature';
@@ -11,4 +16,26 @@ export const reducers: ActionReducerMap<SongsState> = {
   list: fromSongList.reducer,
 };
 
+// 1 Feature Selector
 const selectFeature = createFeatureSelector<SongsState>(FEATURE_NAME);
+
+// 2 One per branch
+
+// 3 Helpers
+
+// 4 What the component needs
+
+export const selectSongListModel = createSelector(() => {
+  const model: SongListModel = {
+    songs: [
+      { id: '1', title: 'Song #1', artist: 'Fugazi' },
+      {
+        id: '2',
+        title: 'National Anthem',
+        artist: 'Radio Head',
+        album: 'Kid A',
+      },
+    ],
+  };
+  return model;
+});
